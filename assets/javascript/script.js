2d4f19aff785517b4caac700eacb3d21234549f6
Cristina Salvo <csalvo@spendsetter.com>
2017-08-07 21:43:50 -0400

    main logic

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,5 @@
-/* global firebase moment */
-// Steps to complete:
 
-// 1. Initialize Firebase
-// 2. Create button for adding new employees - then update the html + update the database
-// 3. Create a way to retrieve employees from the employee database.
-// 4. Create a way to calculate the months worked. Using difference between start and current time.
-//    Then use moment.js formatting to set difference in months.
-// 5. Calculate Total billed
-
+ //Firebase config 
  var config = {
     apiKey: "AIzaSyAe6e-UdD31LbgEmUHQyeE6UmGmdr7zz28",
     authDomain: "timesheet-50c97.firebaseapp.com",
@@ -21,37 +13,52 @@
   var database = firebase.database();
 
 
-
+//on button click
   $("#add-train-btn").on("click", function(){
 	event.preventDefault();
+	//grab values from text fields
 	  var trainName = $("#train-name-input").val();
 	  var trainDestination = $("#destination-input").val();
 	  var firstTrainTime = $("#first-train-input").val();
 	  var trainFrequency = $("#frequency-input").val(); 
-
+	  //set up object to be pushed to the DB
 	  var newTrain = {
 	  	name: trainName,
 	  	destination: trainDestination,
 	  	firstTrain: firstTrainTime,
 	  	frequency: trainFrequency
   }
+ // push object to DB
 	database.ref().push(newTrain);
 
+
+//clear text fields for next input 
 	$("#train-name-input").val("");
 	$("#destination-input").val("");
 	$("#first-train-input").val("");
 	$("#frequency-input").val(""); 
 
-	var formattedMonth = moment(empStart).format("DD/MM/YY")
-	var monthsWorked = moment().format("DD/MM/YY").diff(formattedMonths ,"months");
 
-  	console.log(monthsWorked);
+ 	
   });
 
 
-
   firebase.database().ref().on("child_added", function(snapshot){
+
+  	var trainStart = snapshot.val().firstTrain;
+  	var interval = snapshot.val().frequency;
+
+	var timeToStart = moment().diff(moment(trainStart,"hh:mm A"),'m');
+    var timeToNext = timeToStart % interval;
+    var timeMinsAway = interval - timeToNext;
+    var timeNext = moment().add(timeMinsAway,'m').toDate();
+
+    console.log(timeToNext, timeToStart, timeMinsAway, moment(timeNext).format("hh:mm A"));
+
+
+
     var row = '<tr><td>' + snapshot.val().name + '</td><td>' + snapshot.val().destination +
-                        '</td><td>'+ snapshot.val().frequency + '</td><td></td><td></td></tr><hr>';
+              '</td><td>'+ snapshot.val().frequency + '</td><td>' + moment(timeNext).format("hh:mm A") + '</td><td>' + timeMinsAway + '</td></tr><hr>';
+
  $("#train-table tbody").append(row);
 });
